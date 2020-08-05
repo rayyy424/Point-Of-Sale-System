c32a2967f3ecdec4161098114af758aac787db3e
rayyy424 <rayyy424@users.noreply.github.com>
2020-08-05 20:32:37 +0800

    commit 20200805

--- resources/views/template/header.blade.php
+++ resources/views/template/header.blade.php
@@ -1,6 +1,6 @@
 <header>
     <div class="text-center">
         <h1>Point Of Sale System</h1>
-        <h4>By Lenard Mangay-ayam</h4>
+        <h4>By Speed Freak Sdn Bhd</h4>
     </div>
 </header>
